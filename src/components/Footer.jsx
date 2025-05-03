f5ddffce18a0add457592fc1ea47683214173456
PiusCyre <108563817+PiusCyre@users.noreply.github.com>
2025-05-03 18:40:18 +0100

    footer adjustments

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,16 +2,16 @@ function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="flex flex-col gap-6 text-center h-auto w-full text-white font-bold mb-8 mt-5">
-      <p className="capitalize text-green-400 flex flex-col gap-3 cursor-pointer">
-        <span className="text-white">PieTV+&trade; </span>
+    <footer className="flex flex-col gap-6 text-center h-auto w-full text-white mb-8 mt-5">
+      {/* <p className="capitalize text-green-400 flex flex-col gap-3 cursor-pointer">
+        <span className="text-white"></span>
         <span className="opacity-60 hover:opacity-100">
           made with ❤ by Pius
         </span>
-      </p>
-      <p className="uppercase">
-        All rights reserved <span className="lowercase font-mini">&copy; </span>
-        {currentYear}
+      </p> */}
+      <p className="capitalize">
+        &copy; <span className="lowercase font-serif">{currentYear} </span>
+        PieTV+&trade; All rights reserved.
       </p>
     </footer>
   );
